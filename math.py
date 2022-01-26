5c036c792515ce9c4981825c75e6fea092aed2c6
Rohit Kumar Jaiswal <89607500+rohit4142@users.noreply.github.com>
2022-01-26 22:27:07 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+#add impleentation
+
+def add(x,y):
+    pass
+#subtract impleentation
+def subtract(x,y):
+    pass
+# multiply implementation
+def multiply(x,y):
+    pass
+
+def divide(x,y):
+    pass
